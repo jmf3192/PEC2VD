98e65b5ac329933e5bc4296cff5974d6543d95e0
jmf3192 <153544878+jmf3192@users.noreply.github.com>
2025-04-10 18:48:42 +0200

    Update ACT2_PEC2.py

--- ACT2_PEC2.py
+++ ACT2_PEC2.py
@@ -7,20 +7,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Cargar el dataset
 df = pd.read_csv("Dataset2.csv", sep=';')
 
-# Seleccionar solo columnas numéricas
 df_numeric = df.select_dtypes(include=["number"])
 
-# Calcular la matriz de correlación
 correlation_matrix = df_numeric.corr()
 
-# Crear el mapa de calor de la matriz de correlación
 plt.figure(figsize=(14, 10))
 sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm", square=True, cbar_kws={"shrink": .8})
 plt.title("Matriz de Correlación - Dataset de Ausentismo Laboral")
 plt.tight_layout()
 
-# Guardar la figura
 plt.savefig("matriz_correlacion.png")
